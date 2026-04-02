3c987bff8c25c0a0130e9bbc8e75e963b906e566
YoyoDev0424 <admin@wcystudio.com>
2026-04-02 19:57:10 +0800

    update mainstreet

--- src/scenes/MainStreetScene.js
+++ src/scenes/MainStreetScene.js
@@ -153,7 +153,7 @@ export class MainStreetScene extends Phaser.Scene {
             const bg = this.add.image(currentX, 540, key).setOrigin(0, 0.5).setDepth(1);
             currentX += bg.width; // 累加寬度，讓下一張接在後面
         });
-        const temple = this.add.image(520, 180, 'stage3').setOrigin(0, 0.5).setDepth(10).setScale(0.85);
+        const temple = this.add.image(480, 180, 'stage3').setOrigin(0, 0.5).setDepth(10).setScale(0.9);
 
 
         // 設定相機邊界為總長度 8414px
@@ -218,15 +218,19 @@ export class MainStreetScene extends Phaser.Scene {
 
         const n1 = NpcHelper.createNpc(this, 1, 1900, 200, 1, 'npc1', 7, 'npc1_anim');
 
-        const n1_item = NpcHelper.createNpcItem(this, 1, 1300, 250, 1, 'stage_object_game1', 'stage_object_game1_select', 7);
+        const n1_item = NpcHelper.createNpcItem(this, 1, 1400, 250, 1, 'stage_object_game1', 'stage_object_game1_select', 7);
+
         const n2_item = NpcHelper.createNpcItem(this, 1, 2200, 350, 1, 'stage_object_game2', 'stage_object_game2_select', 7);
         const n3_item = NpcHelper.createNpcItem(this, 1, width / 2 + 380, 1050, 1, 'stage_object_game3', 'stage_object_game3_select', 8);
-        const n4_item = NpcHelper.createNpcItem(this, 1, width / 2 + 100, 350, 1, 'stage_object_game4', 'stage_object_game4_select', 8);
-        const n5_item = NpcHelper.createNpcItem(this, 1, 150, 280, 1, 'stage_object_game5', 'stage_object_game5_select', 7);
+        const n4_item = NpcHelper.createNpcItem(this, 1, width / 2 + 100, 380, 1, 'stage_object_game4', 'stage_object_game4_select', 8);
+        const n5_item = NpcHelper.createNpcItem(this, 1, 180, 280, 1, 'stage_object_game5', 'stage_object_game5_select', 7);
         const n6_item = NpcHelper.createNpcItem(this, 1, width / 2 + 380, 1050, 1, 'stage_object_game6', 'stage_object_game6_select', 7);
         const n7_item = NpcHelper.createNpcItem(this, 1, width / 2 + 950, 650, 1, 'stage_object_game7', 'stage_object_game7_select', 10);
 
-
+        n1_item.setScale(0.8);
+        n4_item.setScale(0.9);
+        n5_item.setScale(0.9);
+        n7_item.setScale(0.9);
 
         this.interactiveNpcs.push(n1);
 
