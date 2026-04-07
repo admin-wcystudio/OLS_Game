73e4cf9d1a2a7c6f37481d8fbb8713a2b9b759d1
YoyoDev0424 <admin@wcystudio.com>
2026-04-08 01:20:48 +0800

    fix

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -155,7 +155,7 @@ export class BootScene extends Phaser.Scene {
 
             this.registry.set('globalSettings', settings);
         }
-        this.scene.start('GameStartScene');
+        this.scene.start('MainStreetScene');
     }
 }
 
